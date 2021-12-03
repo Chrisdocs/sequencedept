73b93872da3cb3b6fac5cd2389f456d77a4e16d3
Chrisdocs <chrisa.wolfe@gmail.com>
2021-12-03 11:38:32 -0600

    working email contact form

--- my-app/src/components/EmailForm/EmailForm.jsx
+++ my-app/src/components/EmailForm/EmailForm.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { useForm } from 'react-hook-form';
+import { init, sendForm } from 'emailjs-com'
+init('user_h5i4jKcOz7z58xA2biMrJ')
 
 
 function EmailForm() {
+    
+    const { handleSubmit, register, watch, formState: {errors} } = useForm();
+    const onSubmit = values => {
+        console.log(values);
+        generateContactNum();
+        
+        sendForm('default_service', 'template_bk8oh9j', '#contact-form')
+        .then(function(response) {
+            console.log('Success!', response.status, response.text);
+        }, function(error) {
+            console.log('Failed...', error);
+        });
+    }
+    
+        const [contactNumber, setContactNumber] = useState("000000");
+    
+        const generateContactNum = () => {
+            const numStr = "000000" + (Math.random() * 1000000 | 0);
+            setContactNumber(numStr.substring(numStr.length - 6));
+        }
 
-    const { handleSubmit, register, formState: {errors} } = useForm();
-    const onSubmit = values => console.log(values);
+    const message = watch('message') || "";
+    const messageCharactersWatch = 250 - message.length;
+
 
     return(
         <div className="contact">
             <h1>Contact</h1>
             <form id='contact-form' onSubmit={handleSubmit(onSubmit)}>
+
+            <input type='hidden' name='contact_number' value={contactNumber} />
                 
+                {errors.username && errors.username.type === "required" && (
+                    <div role="alert">Name is required<br/></div>
+                )}
                 <input 
                     placeholder="Name"
                     aria-invalid={errors.username ? "true" : "false"}
@@ -20,11 +48,11 @@ function EmailForm() {
                         required: true
                         })}
                     />
-                    {errors.username && errors.username.type === "required" && (
-                        <div role="alert">Name is required<br/></div>
-                    )}
                 <br/>
 
+                {errors.email && errors.email.type === "required" && (
+                    <div role="alert">Email is required<br/></div>
+                )}
                 <input 
                 type='email'
                 placeholder='Email'
@@ -35,21 +63,20 @@ function EmailForm() {
                         message: "Invalid email address."
                         } 
                     })}  />
-                    {errors.email && errors.email.type === "required" && (
-                        <div role="alert">Email is required<br/></div>
-                    )}
                 <br/>
 
-                <textarea 
-                name='message'
-                placeholder='Message'
-                {...register('message', {
-                    required: true 
-                    })} 
-                />
                 {errors.message && errors.message.type === "required" && (
                     <div role="alert">A message is required<br/></div>
                 )}
+                <textarea 
+                name='message'
+                placeholder='Message'
+                maxLength='250'
+                {...register('message', {
+                    required: true 
+                })} 
+                />
+                <p className="message-chars">{messageCharactersWatch} characters left</p>
                 <br/>
                 <button type='submit'>Submit</button>
             </form>
