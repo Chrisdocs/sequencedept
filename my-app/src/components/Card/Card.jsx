7d829f9f12cbbe1c676c6bc9f7a0c7573f5bb482
Chrisdocs <chrisa.wolfe@gmail.com>
2021-12-20 18:43:30 -0600

    created video card with title and desc

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -4,9 +4,19 @@ import YoutubeEmbed from "../EmbedVid/Video";
 
 function Cards() {
     return (
-        <Card>
-            <YoutubeEmbed embedId="jRyo0uxQ5DE" />
-        </Card>
+        <div>
+            <Card id="card">
+                <div id="card-video">
+                    <YoutubeEmbed embedId="jRyo0uxQ5DE" />
+                </div>
+                <div id="card-title">
+                    <h2>Title</h2>
+                </div>
+                <div id="card-desc">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quibusdam vel aspernatur sunt ullam voluptate facilis, accusamus porro dolorum placeat libero eveniet, nobis earum incidunt optio illum. Laudantium, obcaecati debitis?</p>
+                </div>
+            </Card>
+        </div>
     );
 }
 
--- my-app/src/pages/About/About.jsx
+++ my-app/src/pages/About/About.jsx
@@ -5,7 +5,7 @@ import Sign from "../../assets/images/SQDPTsign.png"
 function About() {
     return (
         <div id="container-div">
-            <img id="sqdpt-sign" src={Sign} alt="National Park style sign" />
+            <img id="sqdpt-sign" src={Sign} alt="National Park style sign which reads, The Sequence Department serves audiences and clients by taking a holistic approach to producing media.  We produce thoughtfully crafted video sequences inspired by nature, art, history, and humor." />
         </div>
     );
 }
