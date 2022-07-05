27e8c0d38c1c48906ae05a0dea208c983f24a21b
Chrisdocs <chriswolfe.dev@gmail.com>
2022-07-05 16:44:23 -0500

    sendgrid with nodemailer

--- server.js
+++ server.js
@@ -1,96 +1,56 @@
-// const express = require("express");
-// const nodemailer = require("nodemailer");
-// const app = express();
-// const { google } = require("googleapis");
-// const OAuth2 = google.auth.OAuth2;
-// const sgMail = require('@sendgrid/mail')
-// const cors = require("cors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const sendGridTransport = require("nodemailer-sendgrid-transport");
 require('dotenv').config()
-const sgMail = require('@sendgrid/mail');
 
 const PASSWORD = process.env.password;
 const EMAIL = process.env.email;
 const TOKEN = process.env.OAUTH_REFRESH_TOKEN;
 const SECRET = process.env.OAUTH_CLIENT_SECRET;
 const CLIENT = process.env.OAUTH_CLIENTID;
+const SENDGRID_API = process.env.SENDGRID_API_KEY;
 
-// const oauthClient = new OAuth2(
-// 	CLIENT,
-// 	SECRET,
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors());
 
-// 	"https://developers.google.com/oauthplayground"
-// )
+const transporter = nodemailer.createTransport(
+  sendGridTransport({
+    auth: {
+      api_key: SENDGRID_API,
+    },
+  })
+);
 
-// oauthClient.setCredentials({
-// 	refresh_token: TOKEN
-// });
+ transporter.verify((err, success) => {
+	err
+		? console.log(err)
+		: console.log(`=== Server is ready to take messages: ${success} ===`);
+ });
 
-// const accessToken = oauthClient.getAccessToken()
-
-// // middleware
-// app.use(express.json());
-// app.use(cors());
-
-// const port = process.env.PORT || 3001;
-// app.listen(port, () => {
-// 	console.log(`Server is running on port: ${port}`)
-// });
-
-// let transporter = nodemailer.createTransport({
-// 	service: "gmail",
-// 	auth: {
-// 		type: "OAuth2",
-// 		user: EMAIL,
-// 		pass: PASSWORD,
-// 		clientId: CLIENT,
-// 		clientSecret: SECRET,
-// 		refreshToken: TOKEN,
-// 		accessToken: accessToken
-// 	},
-//  });
-
-//  transporter.verify((err, success) => {
-// 	err
-// 		? console.log(err)
-// 		: console.log(`=== Server is ready to take messages: ${success} ===`);
-//  });
-
-//  app.post("/send", function (req, res) {
-// 	let mailOptions = {
-// 		from: `${req.body.mailerState.email}`,
-// 		to: EMAIL,
-// 		subject: `${req.body.mailerState.subject} from ${req.body.mailerState.name}`,
-// 		text: `You received this email from ${req.body.mailerState.name} @ ${req.body.mailerState.email} They wrote: \n${req.body.mailerState.message}`,
-// 	};
- 
-// 	transporter.sendMail(mailOptions, function (err, data) {
-// 		if (err) {
-// 			console.log("Error " + err, "data " + data);
-// 			res.json({
-// 				status: "fail"
-// 			});
-// 		} else {
-// 			console.log("Email sent successfully");
-// 			res.json({ status: "success" });
-// 		}
-// 	});
-//  });
-
-
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-		from: `${req.body.mailerState.email}`,
+ app.post("/send", function (req, res) {
+	let mailOptions = {
 		to: EMAIL,
+		from: 'Contact.sqdpt@gmail.com',
 		subject: `${req.body.mailerState.subject} from ${req.body.mailerState.name}`,
 		text: `You received this email from ${req.body.mailerState.name} @ ${req.body.mailerState.email} They wrote: \n${req.body.mailerState.message}`,
-};
+	};
 
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+	transporter.sendMail(mailOptions, function (err, data) {
+		if (err) {
+			console.log("Error " + err, "data " + data);
+			res.json({
+				status: "fail"
+			});
+		} else {
+			console.log("Email sent successfully");
+			res.json({ status: "success" });
+		}
+	});
+ });
+
+app.listen(PORT, () => {
+  console.log("server is running on", PORT);
+});
