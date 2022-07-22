fee6d67d1ff09f076da7f71b3961316f8c6331e7
Chrisdocs <chriswolfe.dev@gmail.com>
2022-07-22 09:35:25 -0500

    project section changes

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 function Navigation() {
 
+	// detect top of page
+	const [isTop, setIsTop] = useState(true);
+	const [isScrolled, setIsScrolled] = useState(false);
 
-window.addEventListener("scroll", function() {
-	
-    let elementTarget = this.document.getElementById("header");
+	console.log("isTop", isTop);
+	console.log("isScrolled", isScrolled);
 
-    if (this.window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-50px";
-    }
-})
+// detect top of page
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 0) {
+			setIsTop(false);
+			setIsScrolled(true);
+		} else {
+			setIsTop(true);
+			setIsScrolled(false);
+		}
+	}
+	);
 
-    return (
-        <div>
-            <section>
-                        <div id="navbar">
-                            <div id="navbar-right">
-                                <a className='nav-text active' href={"#header"}>SQ-DPT</a>
-                                <a className='nav-text' href={"#about-page"}>About</a>
-                                <a className='nav-text' href={"#project-page"}>Projects</a>
-                                <a className='nav-text' href={"#contact-page"}>Contact</a>
-                            </div>
-                        </div>
-            </section>
+
+  return (
+    <div>
+      <section>
+        <div id="navbar">
+          <div id="navbar-right">
+            <a className="nav-text active" href={"#header"}>
+              SQ-DPT
+            </a>
+            <a className="nav-text" href={"#about-page"}>
+              About
+            </a>
+            <a className="nav-text" href={"#project-page"}>
+              Projects
+            </a>
+            <a className="nav-text" href={"#contact-page"}>
+              Contact
+            </a>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default Navigation;
